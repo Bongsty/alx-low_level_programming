58c75c7518800745245a61c02f7cd0b4d072fd90
Bongsty <mbmoabi@outlook.com>
2023-03-27 16:51:54 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 int _atoi(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
-int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
