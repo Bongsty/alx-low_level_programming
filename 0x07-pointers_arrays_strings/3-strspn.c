bdc2661eccfff6523ec9ee83d5828182d285a3f1
Bongsty <mbmoabi@outlook.com>
2023-04-03 16:12:05 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: Input
+ * @accept: Input
+ * Return: Alwaays 0(Success)
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+	unsigned int g = 0;
+	int h;
+
+	while (*s)
+	{
+		for (h = 0; accept[h]; h++)
+		{
+			if (*s == accept[h])
+			{
+				g++;
+				break;
+			}
+			else if (accept[g + 1] == '\0)
+				return (n);
+		}
+		s++;
+	}
+	return (g);
+	}
